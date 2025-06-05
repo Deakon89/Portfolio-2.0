f48e085cafc63fbee660c6e0b7414fb0d43b42e8
Deakon89 <franck.devw@gmail.com>
2025-06-05 11:19:35 +0200

    add backgroung

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
-import { HeroComponent } from './component/hero/hero.component';
-import { ProjectsPageComponent } from './component/project-page/project-page.component';
+
 
 
 export const routes: Routes = [
-    {
-        path: '',loadComponent: () => import('./component/hero/hero.component').then(m => m.HeroComponent), children: [
-            {path: 'project-page', component:ProjectsPageComponent}
-        ]
-    },
+   {
+    path: '',
+    loadComponent: () => import('./component/home/home.component').then(m => m.HomeComponent)
+  }
     
 ]
