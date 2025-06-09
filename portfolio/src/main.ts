36297d69fc915a15b8e7e8f9eedd72f6bdcb096b
Deakon89 <franck.devw@gmail.com>
2025-06-09 21:19:56 +0200

    splash

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -3,7 +3,11 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 bootstrapApplication(AppComponent,{
-providers: [provideRouter(routes)]
+providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule)
+]
  }) .catch((err) => console.error(err));
