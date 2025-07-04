ed36b84b3bd2f399ea6509cc32deeec6122b4bfd
Deakon89 <franck.devw@gmail.com>
2025-07-04 23:09:28 +0200

    fix project

--- portfolio/src/app/component/projects/projects.component.ts
+++ portfolio/src/app/component/projects/projects.component.ts
@@ -14,10 +14,10 @@ export class ProjectsComponent implements OnInit {
   loading = true;
 
     projects = [
-    { title: 'RunSpechs', img: 'assets/run.png', description: 'running app', link: 'https://github.com/Deakon89' },
-    { title: 'WarePulse',  img: 'assets/wp.png',  description: 'warehouse app' , link: 'https://github.com/Deakon89'},
-    { title: 'Fenchina mani di carta', img: 'assets/fench.png', description: 'E-Commerce' , link: 'https://github.com/Deakon89'},
-    { title: 'Inchiostro', img: 'assets/inc.png', description: 'Open-library app' , link: 'https://github.com/Deakon89'},
+    { title: 'RunSpechs', img: 'assets/run.png', description: 'running app', link: 'https://runspechs.netlify.app/home' },
+    // { title: 'WarePulse',  img: 'assets/wp.png',  description: 'warehouse app' , link: 'https://github.com/Deakon89'},
+    { title: 'Fenchina mani di carta', img: 'assets/fench.png', description: 'E-Commerce' , link: 'https://fenchinamanidicarta.netlify.app'},
+    { title: 'Inchiostro', img: 'assets/inc.png', description: 'Open-library app' , link: 'https://inchiostrolibrary.netlify.app'},
     // … altri progetti …
   ];
 
@@ -31,14 +31,10 @@ export class ProjectsComponent implements OnInit {
   } 
 
   prev() {
-      if (this.currentIndex > 0) {
-        this.currentIndex--;
-      }
-    }
+    this.currentIndex = Math.max(this.currentIndex - 1, 0);
+  }
   
     next() {
-      if (this.currentIndex < this.projects.length - 1) {
-        this.currentIndex++;
-      }
+      this.currentIndex = Math.min(this.currentIndex + 1, this.projects.length - 1);
     }
 }
